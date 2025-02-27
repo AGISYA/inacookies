6516296a8d4f247b489609ae4b92a3a1a1a0603f
AGISYA <abdiagisya@gmail.com>
2025-02-27 16:00:40 +0700

    update discount and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Header from "@/components/header";
 import HeroBanner from "@/components/hero-baner";
 import ImageSection from "@/components/images-section";
 import ProductShowcase from "@/components/peoduct-showcase";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -19,6 +20,14 @@ export default function Home() {
       </section>
       <section id="shop">
         <ProductShowcase />
+        <div className="flex justify-center mt-6">
+          <Link
+            href="/products"
+            className="px-6 py-2 text-white bg-red-600 rounded-lg shadow-lg hover:bg-yellow-500 transition"
+          >
+            Lihat Produk Lainnya
+          </Link>
+        </div>
       </section>
       <section id="advantages">
         <AdvantagesSection />
